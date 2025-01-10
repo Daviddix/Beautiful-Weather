aed3a2fc0fc1da1c6468513580bb7fe9324a2a27
Emmanuel Nsikan-David <en56434@gmail.com>
2025-01-10 08:10:49 +0100

    finished basic functionality for the homepage

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,20 +4,99 @@ import OtherWeatherInfo from "./Components/OtherWeatherInfo/OtherWeatherInfo";
 import SearchButton from "./Components/SearchButton/SearchButton";
 import ForecastContainer from "./Components/ForecastContainer/ForecastContainer";
 import SearchModal from "./Components/SearchModal/SearchModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddedCountries from "./Components/AddedCountries/AddedCountries";
+
+enum ComponentStates {
+  loading = "loading",
+  completed = "completed",
+  error = "error"
+}
+
+interface MainWeatherInfoProps {
+  country: string,
+  state: string,
+  shortDescription: string,
+  longDescription: string,
+  degree: number,
+}
+
+interface SubWeatherInfoProps {
+  wind: number,
+  pressure: number,
+  humidity: number,
+}
 
 function Home() {
   const [showSearchModal, setShowSearchModal] = useState(false)
+  const [mainWeatherInfo, setMainWeatherInfo] = useState<MainWeatherInfoProps>({
+    country: "",
+    state: "",
+    shortDescription: "",
+    longDescription: "",
+    degree: 0
+  })
+  const [subWeatherInfo, setSubWeatherInfo] = useState<SubWeatherInfoProps>({
+    humidity: 0,
+    wind: 0,
+    pressure: 0
+  })
+  const [componentState, setComponentState] = useState(ComponentStates.loading)
+
+  useEffect(() => {
+    fetchWeatherInformation()
+  }, [])
+
+  async function fetchWeatherInformation() {
+    try {
+      const rawFetch = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.7749&lon=-122.4194&units=metric&exclude={part}&appid=5df3b8dda637f8873722662b50a8a9c1")
+      const response = await rawFetch.json()
+
+      const newMainWeatherInfo = {
+        country: response.sys.country,
+        state: response.name,
+        shortDescription: response.weather[0].main,
+        longDescription: response.weather[0].description,
+        degree: response.main.temp,
+      }
+
+      const newSubWeatherInfo = {
+        wind: response.wind.speed,
+        pressure: response.main.pressure,
+        humidity: response.main.humidity,
+      }
+
+      setMainWeatherInfo(newMainWeatherInfo)
+      setSubWeatherInfo(newSubWeatherInfo)
+      setComponentState(ComponentStates.completed)
+
+      if (!rawFetch.ok) {
+        throw new Error("Server error")
+      }
+    }
+    catch (err) {
+      setComponentState(ComponentStates.error)
+      console.log(err)
+    }
+  }
+
   return (
     <main>
       <div className="main-weather-section">
         <div className="inner">
-          <SearchButton setShowSearchModal={setShowSearchModal}/>
+          <SearchButton setShowSearchModal={setShowSearchModal} />
 
-          <WeatherInfo />
+          <WeatherInfo
+            country={mainWeatherInfo.country}
+            degree={mainWeatherInfo.degree}
+            longDescription={mainWeatherInfo.longDescription}
+            state={mainWeatherInfo.state}
+            shortDescription={mainWeatherInfo.shortDescription} />
 
-          <OtherWeatherInfo />
+          <OtherWeatherInfo
+            wind={subWeatherInfo.wind}
+            pressure={subWeatherInfo.pressure}
+            humidity={subWeatherInfo.humidity} />
 
         </div>
       </div>
